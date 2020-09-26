ae8d608a0978166389ce124502ed872bc1844be8
romak <dieci.drago@gmail.com>
2020-09-26 23:21:45 +0900

    taskモデルの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :boards
+  has_many :boards, dependent: :destroy
+  has_many :tasks, dependent: :destroy
   
 end
